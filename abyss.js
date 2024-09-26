53aa6bc5d23fe470b5d088fcd37fa029a700934e
Anthrasil <raphaelchristopher.dorninger@gmail.com>
2024-09-26 21:24:11 +0200

    fixed a bug

--- abyss.js
+++ abyss.js
@@ -2,16 +2,14 @@ let monitor = document.querySelector("#monitor");
 let UIstats = {
     "open": true,
 }
-setInterval(function () {
-    rutin(monitor, UIstats);
-}, 1)
+rutin(monitor, UIstats);
 function rutin(monitor, UIstats) {
     handleUIEvents(monitor, UIstats);
 }
 function handleUIEvents(monitor, stats) {
-
     let /**@type {HTMLElement} */inventory = monitor.querySelector("#inventory");
     inventoryEvent(stats, inventory);
+    console.log(document.event);
 }
 function refreshShop(inventory) {
     let refresh = inventory.querySelector("#Refresh");
@@ -26,11 +24,10 @@ function openClose(stats, inventory) {
     let openclose = inventory.querySelector("#open-close");
     openclose.addEventListener("click", (event) => {
         stats.open = !stats.open
-    }, { once: true })
-    if (stats.open && inventory.classList.contains("closed")) {
-        inventory.classList.replace("closed", "opened")
-    }
-    if (!stats.open && inventory.classList.contains("opened")) {
-        inventory.classList.replace("opened", "closed")
-    }
+        if (stats.open && inventory.classList.contains("closed")) {
+            inventory.classList.replace("closed", "opened")
+        } else if (!stats.open && inventory.classList.contains("opened")) {
+            inventory.classList.replace("opened", "closed")
+        }
+    })
 }
